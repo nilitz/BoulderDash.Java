a760c476a03f2ae27942b75b806d7248e90539c3
valentin <valentin.reinbold@viacesi.fr>
2019-05-23 11:26:48 +0200

    remise a 0

--- main/src/main/java/main/Main.java
+++ main/src/main/java/main/Main.java
@@ -22,10 +22,7 @@ public abstract class Main {
      * @param args
      *            the arguments
      */
-    /*
-    * TEST
-    *
-     */
+
     public static void main(final String[] args) {
         final Model model = new Model();
         final View view = new View(model);
