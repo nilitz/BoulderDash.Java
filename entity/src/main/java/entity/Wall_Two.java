81265fc99a4209836649068386bfbd77a3e9f6ed
Reinbold Valentin <44491813+Tarru@users.noreply.github.com>
2019-05-31 17:19:43 +0200

    Create Wall_Two.java

---
+++ entity/src/main/java/entity/Wall_Two.java
@@ -0,0 +1,16 @@
+package entity;
+/**
+ * The class Wall_Two
+ * @author HugoDegrossi
+ */
+public class Wall_Two extends Object {
+    /**
+     * Constructor
+     * @param name
+     * @param status
+     * @param lastMove
+     */
+    public Wall_Two(String name, boolean status, LastMove lastMove) {
+        super(name, status, lastMove);
+    }
+}
