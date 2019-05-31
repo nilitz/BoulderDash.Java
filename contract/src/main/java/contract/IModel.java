8036575b8412249f6ce8cdfa3dcea86308581893
Reinbold Valentin <44491813+Tarru@users.noreply.github.com>
2019-05-31 16:21:25 +0200

    Update IModel.java
    Added some methods and Javadoc Commentary

--- contract/src/main/java/contract/IModel.java
+++ contract/src/main/java/contract/IModel.java
@@ -1,30 +1,23 @@
 package contract;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Observable;
 
-import entity.HelloWorld;
+import entity.MapTile;
 
 /**
  * The Interface IModel.
  *
- * @author Jean-Aymeric Diet
+ * @author Hugo Degrossi
+ *
  */
 public interface IModel {
-
 	/**
-	 * Gets the hello world.
+	 * ArrayList which contains the mapTiles
 	 *
-	 * @return the helloworld entity
 	 */
-	HelloWorld getHelloWorld();
-
-	/**
-	 * Load the message.
-	 *
-	 * @param code
-	 *          the code
-	 */
-	void loadHelloWorld(String code);
+	ArrayList<MapTile> getMap();
 
 	/**
 	 * Gets the observable.
@@ -32,4 +25,100 @@ public interface IModel {
 	 * @return the observable
 	 */
 	Observable getObservable();
+
+	/** Set the map
+	 *
+	 * @param ID
+	 * @throws SQLException
+	 */
+	void setMap(int ID) throws SQLException;
+
+	/**Getter  of  ID
+	 *
+	 *
+	 */
+
+	int getID();
+
+	/**
+	 * Setter of ID
+	 * @param ID
+	 */
+
+	void setID(int ID);
+
+	/**
+	 * Get size from the map
+	 * @return
+	 * @throws SQLException
+	 */
+	int[] getSize() throws SQLException;
+
+	/**
+	 * Get the size in width of the map
+	 * @return
+	 * @throws SQLException
+	 */
+	int getMapWidth() throws SQLException;
+
+	/**
+	 * get the size in Height of the map
+	 *
+	 * @throws SQLException
+	 */
+    int getMapHeight() throws SQLException;
+
+	/**
+	 * Get the controller Order and load it
+	 * @param controllerOrder
+	 */
+	void loadPlayerControl(ControllerOrder controllerOrder);
+
+	/**
+	 * Take the direction and make the movement of the player
+	 * @param direction
+	 * @throws SQLException
+	 */
+	void movement(String direction) throws SQLException;
+
+	/**
+	 * Get the index of an ennemy and moves it
+	 * @param index
+	 * @throws SQLException
+	 * @throws InterruptedException
+	 */
+	void enemyAutoMove(int index) throws SQLException, InterruptedException;
+
+	/**
+	 * Get the index of a rock or a diamon and automoves it
+	 * @param index
+	 * @throws SQLException
+	 * @throws InterruptedException
+	 */
+	void autoMove(int index) throws SQLException, InterruptedException;
+
+	/**
+	 * Get the status of a player
+	 *
+	 */
+	boolean playerStatus();
+
+	/**
+	 * Getter  of Win
+	 *
+	 */
+	boolean getWin();
+
+	/**
+	 * Setter of Win
+	 * @param win
+	 */
+	void setWin(boolean win);
+
+	/**
+	 * Setter of Diamond Counter
+	 * @param diamondCounter
+	 */
+	void setDiamondCounter(int diamondCounter);
+
 }
