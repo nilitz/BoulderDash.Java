bf62caa3264adc1cc18597bc84430c00ba91c2fb
Reinbold Valentin <44491813+Tarru@users.noreply.github.com>
2019-05-31 17:16:29 +0200

    Update MapTile.java

--- entity/src/main/java/entity/MapTile.java
+++ entity/src/main/java/entity/MapTile.java
@@ -1,36 +1,76 @@
 package entity;
-
+/**
+ * The class MapTile
+ * @author HugoDegrossi
+ */
 public class MapTile {
-    private String name;
+    private Object object;
     private int x;
     private int y;
 
-    public MapTile(String name, int x, int y){
-        this.setName(name);
+    /**
+     * Constructor
+     * @param object
+     * @param x
+     * @param y
+     */
+    public MapTile(Object object, int x, int y){
+        this.setObject(object);
         this.setX(x);
         this.setY(y);
     }
 
-    public void setName(String name) {
-        this.name = name;
+    /**
+     * Print the MapTile in the Console
+     */
+    public void printMapTile(){
+        System.out.println(this.getObject().getName() + " | " + this.getX() + " | " + this.getY());
     }
 
+    /**
+     * Setter of Object
+     * @param object
+     */
+    public void setObject(Object object) {
+        this.object = object;
+    }
+
+    /**
+     * Setter of X
+     * @param x
+     */
     public void setX(int x) {
         this.x = x;
     }
 
+    /**
+     * Setter of Y
+     * @param y
+     */
     public void setY(int y) {
         this.y = y;
     }
 
-    public String getName() {
-        return name;
+    /**
+     * Getter of object
+     * @return
+     */
+    public Object getObject() {
+        return this.object;
     }
 
+    /**
+     * Getter of X
+     * @return
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * Getter of Y
+     * @return
+     */
     public int getY() {
         return y;
     }
