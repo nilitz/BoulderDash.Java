5dcc90fa97303050054a122f33a045c8387d1000
Reinbold Valentin <44491813+Tarru@users.noreply.github.com>
2019-05-31 17:26:46 +0200

    Update Controller.java
    Adding Javadoc commentary

--- controller/src/main/java/controller/Controller.java
+++ controller/src/main/java/controller/Controller.java
@@ -4,9 +4,9 @@ import contract.ControllerOrder;
 import contract.IController;
 import contract.IModel;
 import contract.IView;
-import org.graalvm.compiler.loop.InductionVariable;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.sql.SQLException;
 
 /**
  * The Class Controller.
@@ -18,6 +18,7 @@ public final class Controller implements IController {
 
 	/** The model. */
 	private IModel	model;
+
 
 	/**
 	 * Instantiates a new controller.
@@ -33,17 +34,25 @@ public final class Controller implements IController {
 	}
 
 	/**
-     * Control.
-     */
-	/*
-	 * (non-Javadoc)
+	 * this print messages with the controls
 	 *
-	 * @see contract.IController#control()
 	 */
 	public void control() {
-		this.view.printMessage("Appuyer sur les touches 'E', 'F', 'D' ou 'I', pour afficher Hello world dans la langue d votre choix.");
+		this.view.printMessage("----- Controls -----" + "\n" + "| UP : Z | DOWN : S | LEFT : Q | RIGHT : D |" + "\n" + "-----Credits -----" + "\n" + " Group 8 ");
 	}
 
+	/**
+	 * Print the message when game is over
+	 */
+	public void death(){
+		this.view.printMessage("GAME OVER");
+	}
+
+	/**
+	 * Print the message when Won
+	 */
+	public void win(){this.view.printMessage("WINNER WINNER CHICKEN DINNER");
+	}
 	/**
      * Sets the view.
      *
@@ -65,68 +74,92 @@ public final class Controller implements IController {
 	}
 
 	/**
-     * Order perform.
-     *
-     * @param controllerOrder
-     *            the controller order
-     */
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see contract.IController#orderPerform(contract.ControllerOrder)
+	 * Set the game on
+	 * @throws InterruptedException
+	 * @throws SQLException
+	 * @throws IOException
 	 */
-	
-
-	public void moveUpDown(int index){
-		if (mapTiles.get(index).getName().equals("Ground_One") || mapTiles.get(index).getName().equals("Ground_Two")){
-			mapTiles.get(index + this.direction).setName("Player_One");
-			mapTiles.get(index).setName("Ground_Two");
+	public void play() throws InterruptedException, SQLException, IOException {
+		int i = 1;
+		control();
+		while(i == 1){
+			i = autoMoveController();
+			orderPerform(ControllerOrder.NOTHING);
+			if (i == 0){death();}
+			if (i == 2){
+				win();
+				nextLevel();
+				i = 1;
+			}
 		}
 	}
 
+	/**
+	 * Initiate a new level
+	 * @throws IOException
+	 * @throws SQLException
+	 */
+	public void nextLevel() throws IOException, SQLException {
+		this.model.setDiamondCounter(0);
+		this.model.setWin(false);
+		this.model.setID(this.model.getID() + 1);
+		this.model.setMap(this.model.getID());
+	}
 
-	public void moveLeftRight(int index){
-		if (mapTiles.get(index + this.direction).getName().equals("Rock")){
-			if (mapTiles.get(index + (2 * this.direction)).getName().equals("Ground_Two")){
-				mapTiles.get(index + this.direction).setName("Player_One");
-				mapTiles.get(index + (2 * this.direction)).setName("Rock");
-				mapTiles.get(index).setName("Ground_Two");
-
-			}
-
+	/**
+	 * Control the autoMove of the enemy or the diamond
+	 * @return
+	 * @throws InterruptedException
+	 * @throws SQLException
+	 * @throws IOException
+	 */
+	public int autoMoveController() throws InterruptedException, SQLException, IOException {
+		for (int index = 361 - 1; index >= 0; index--){
+			this.model.autoMove(index);
 		}
-		else if (mapTiles.get(index).getName().equals("Ground_One") || mapTiles.get(index).getName().equals("Ground_Two")){
-			mapTiles.get(index + this.direction).setName("Player_One");
-			mapTiles.get(index).setName("Ground_Two");
+		this.setView(view);
+		Thread.sleep(150);
+
+		if(this.model.playerStatus()){
+			return 0;
+		}
+		else if(this.model.getWin()){
+			return 2;
+		}
+		else{
+			return 1;
 		}
 
 	}
 
-	public void orderPerform(final ControllerOrder controllerOrder) {
-		for (int i = 0; i <= mapTiles.size(); i++){
-			if (mapTiles.get(i).getName()=="Player"){
-				Direction direction;
-				switch (controllerOrder) {
-					case Up:
-						direction = Direction.Up;
-						break;
-					case Down:
-						direction = Direction.Down;
-						break;
-					case Left:
-						direction = Direction.Left;
-						break;
-					case Right:
-						direction = Direction.Right;
-						break;
-					case Nothing:
-						direction = Direction.Nothing;
-						break;
+	/**
+	 * Perform the order given by the model.
+	 * @param controllerOrder
+	 * @throws SQLException
+	 * @throws IOException
+	 */
+	public void orderPerform(final ControllerOrder controllerOrder) throws SQLException, IOException {
 
-					}
-				}
-			}
+		switch (controllerOrder) {
+			case UP:
+				this.model.movement("UP");
+				break;
+			case DOWN:
+				this.model.movement("DOWN");
+				break;
+			case RIGHT:
+				this.model.movement("RIGHT");
+				break;
+			case LEFT:
+				this.model.movement("LEFT");
+				break;
+			case NOTHING:
+				this.model.movement("NOTHING");
+				break;
+			default:
+				break;
 		}
+		this.setView(view);
 	}
 
 }
