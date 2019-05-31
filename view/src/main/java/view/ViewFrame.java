98933f001558c1e38b99a5ff6a2dafc36af5abd8
Reinbold Valentin <44491813+Tarru@users.noreply.github.com>
2019-05-31 17:46:01 +0200

    Update ViewFrame.java

--- view/src/main/java/view/ViewFrame.java
+++ view/src/main/java/view/ViewFrame.java
@@ -4,6 +4,8 @@ import java.awt.GraphicsConfiguration;
 import java.awt.HeadlessException;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.IOException;
+import java.sql.SQLException;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -14,153 +16,171 @@ import contract.IModel;
 /**
  * The Class ViewFrame.
  *
- * @author Jean-Aymeric Diet
+ * @author HugoDegrossi
  */
 class ViewFrame extends JFrame implements KeyListener {
 
-    /**
-     * The model.
-     */
-    private IModel model;
+	/**
+	 * The model.
+	 */
+	private IModel model;
 
-    /**
-     * The controller.
-     */
-    private IController controller;
-    /**
-     * The Constant serialVersionUID.
-     */
-    private static final long serialVersionUID = -697358409737458175L;
+	/**
+	 * The controller.
+	 */
+	private IController controller;
+	/**
+	 * The Constant serialVersionUID.
+	 */
+	private static final long serialVersionUID = -697358409737458175L;
 
-    /**
-     * Instantiates a new view frame.
-     *
-     * @param model the model
-     * @throws HeadlessException the headless exception
-     */
-    public ViewFrame(final IModel model) throws HeadlessException {
-        this.buildViewFrame(model);
-    }
+	/**
+	 * Instantiates a new view frame.
+	 *
+	 * @param model the model
+	 * @throws HeadlessException the headless exception
+	 */
+	public ViewFrame(final IModel model) throws HeadlessException, IOException {
+		this.buildViewFrame(model);
+	}
 
-    /**
-     * Instantiates a new view frame.
-     *
-     * @param model the model
-     * @param gc    the gc
-     */
-    public ViewFrame(final IModel model, final GraphicsConfiguration gc) {
-        super(gc);
-        this.buildViewFrame(model);
-    }
+	/**
+	 * Instantiates a new view frame.
+	 *
+	 * @param model the model
+	 * @param gc    the gc
+	 */
+	public ViewFrame(final IModel model, final GraphicsConfiguration gc) throws IOException {
+		super(gc);
+		this.buildViewFrame(model);
+	}
 
-    /**
-     * Instantiates a new view frame.
-     *
-     * @param model the model
-     * @param title the title
-     * @throws HeadlessException the headless exception
-     */
-    public ViewFrame(final IModel model, final String title) throws HeadlessException {
-        super(title);
-        this.buildViewFrame(model);
-    }
+	/**
+	 * Instantiates a new view frame.
+	 *
+	 * @param model the model
+	 * @param title the title
+	 * @throws HeadlessException the headless exception
+	 */
+	public ViewFrame(final IModel model, final String title) throws HeadlessException, IOException {
+		super(title);
+		this.buildViewFrame(model);
+	}
 
-    /**
-     * Instantiates a new view frame.
-     *
-     * @param model the model
-     * @param title the title
-     * @param gc    the gc
-     */
-    public ViewFrame(final IModel model, final String title, final GraphicsConfiguration gc) {
-        super(title, gc);
-        this.buildViewFrame(model);
-    }
+	/**
+	 * Instantiates a new view frame.
+	 *
+	 * @param model the model
+	 * @param title the title
+	 * @param gc    the gc
+	 */
+	public ViewFrame(final IModel model, final String title, final GraphicsConfiguration gc) throws IOException {
+		super(title, gc);
+		this.buildViewFrame(model);
+	}
+	/**
+	 * Builds the view frame.
+	 *
+	 * @param model the model
+	 */
+	private void buildViewFrame(final IModel model) throws IOException {
+		this.setModel(model);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.addKeyListener(this);
+		this.setContentPane(new ViewPanel(this));
+		this.setSize(1000 + this.getInsets().left + this.getInsets().right, 1000 + this.getInsets().top + this.getInsets().bottom);
+		this.setLocationRelativeTo(null);
+	}
 
-    /**
-     * Gets the controller.
-     *
-     * @return the controller
-     */
-    private IController getController() {
-        return this.controller;
-    }
+	/**
+	 * Gets the controller.
+	 *
+	 * @return the controller
+	 */
+	private IController getController() {
+		return this.controller;
+	}
 
-    /**
-     * Sets the controller.
-     *
-     * @param controller the new controller
-     */
-    protected void setController(final IController controller) {
-        this.controller = controller;
-    }
+	/**
+	 * Sets the controller.
+	 *
+	 * @param controller
+	 *          the new controller
+	 */
+	protected void setController(final IController controller) {
+		this.controller = controller;
+	}
 
-    /**
-     * Gets the model.
-     *
-     * @return the model
-     */
-    protected IModel getModel() {
-        return this.model;
-    }
+	/**
+	 * Gets the model.
+	 *
+	 * @return the model
+	 */
+	protected IModel getModel() {
+		return this.model;
+	}
 
-    /**
-     * Sets the model.
-     *
-     * @param model the new model
-     */
-    private void setModel(final IModel model) {
-        this.model = model;
-    }
+	/**
+	 * Sets the model.
+	 *
+	 * @param model
+	 *          the new model
+	 */
+	private void setModel(final IModel model) {
+		this.model = model;
+	}
 
-    /**
-     * Builds the view frame.
-     *
-     * @param model the model
-     */
-    private void buildViewFrame(final IModel model) {
-        this.setModel(model);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setResizable(false);
-        this.addKeyListener(this);
-        this.setContentPane(new ViewPanel(this));
-        this.setSize(400 + this.getInsets().left + this.getInsets().right, 60 + this.getInsets().top + this.getInsets().bottom);
-        this.setLocationRelativeTo(null);
-    }
 
-    /**
-     * Prints the message.
-     *
-     * @param message the message
-     */
-    public void printMessage(final String message) {
-        JOptionPane.showMessageDialog(null, message);
-    }
+	/**
+	 * Prints the message.
+	 *
+	 * @param message
+	 *          the message
+	 */
+	public void printMessage(final String message) {
+		JOptionPane.showMessageDialog(null, message);
+	}
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see java.awt.event.KeyListener#keyTyped(java.awt.event.KeyEvent)
-     */
-    public void keyTyped(final KeyEvent e) {
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.awt.event.KeyListener#keyTyped(java.awt.event.KeyEvent)
+	 */
+	public void keyTyped(final KeyEvent e) {
 
-    }
+	}
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see java.awt.event.KeyListener#keyPressed(java.awt.event.KeyEvent)
-     */
-    public void keyPressed(final KeyEvent e) {
-        this.getController().orderPerform(View.keyCodeToControllerOrder(e.getKeyCode()));
-    }
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.awt.event.KeyListener#keyPressed(java.awt.event.KeyEvent)
+	 */
+	public void keyPressed(final KeyEvent e) {
+		try {
+			this.getController().orderPerform(View.keyCodeToControllerOrder(e.getKeyCode()));
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.awt.event.KeyListener#keyReleased(java.awt.event.KeyEvent)
+	 */
+	public void keyReleased(final KeyEvent e) {
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see java.awt.event.KeyListener#keyReleased(java.awt.event.KeyEvent)
-     */
-    public void keyReleased(final KeyEvent e) {
+	}
 
-    }
+	/**
+	 * Adapt the window to your screen
+	 * @throws SQLException
+	 */
+	public void adaptWindow() throws SQLException {
+		int[] temp = this.getModel().getSize();
+
+		this.setSize( temp[1] * 16 * 3 + this.getInsets().left + this.getInsets().right,  temp[0] * 16 * 3 + this.getInsets().top + this.getInsets().bottom);
+	}
 }
