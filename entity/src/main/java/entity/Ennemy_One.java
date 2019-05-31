ebfc9133338628da4155ec5b8c6a289d642dcaa7
Reinbold Valentin <44491813+Tarru@users.noreply.github.com>
2019-05-31 17:17:31 +0200

    Create Ennemy_One.java

---
+++ entity/src/main/java/entity/Ennemy_One.java
@@ -0,0 +1,16 @@
+package entity;
+/**
+ * The class Ennemy_One
+ * @author HugoDegrossi
+ */
+public class Enemy_One extends Object {
+    /**
+     * Constructor
+     * @param name
+     * @param status
+     * @param lastMove
+     */
+    public Enemy_One(String name, boolean status, LastMove lastMove) {
+        super(name, status, lastMove);
+    }
+}
