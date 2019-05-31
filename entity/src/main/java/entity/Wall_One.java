cdd1520de02336c95bccc2c3e75bb9bfd5c6bcda
Reinbold Valentin <44491813+Tarru@users.noreply.github.com>
2019-05-31 17:19:28 +0200

    Create Wall_One.java

---
+++ entity/src/main/java/entity/Wall_One.java
@@ -0,0 +1,16 @@
+package entity;
+/**
+ * The class  Wall_One
+ * @author HugoDegrossi
+ */
+public class Wall_One extends Object {
+    /**
+     * Constructor
+     * @param name
+     * @param status
+     * @param lastMove
+     */
+    public Wall_One(String name, boolean status, LastMove lastMove) {
+        super(name, status, lastMove);
+    }
+}
