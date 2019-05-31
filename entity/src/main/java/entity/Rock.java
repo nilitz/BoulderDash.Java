0032dd2a7f1a6f89ac5610e02fc22a3b93348427
Reinbold Valentin <44491813+Tarru@users.noreply.github.com>
2019-05-31 17:19:14 +0200

    Create Rock.java

---
+++ entity/src/main/java/entity/Rock.java
@@ -0,0 +1,16 @@
+package entity;
+/**
+ * The class  Rock
+ * @author HugoDegrossi
+ */
+public class Rock extends Object {
+    /**
+     * Constructor
+     * @param name
+     * @param status
+     * @param lastMove
+     */
+    public Rock(String name, boolean status, LastMove lastMove) {
+        super(name, status, lastMove);
+    }
+}
