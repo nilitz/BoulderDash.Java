9221e993e64c5ee5af62532624a68e18bde810d3
Reinbold Valentin <44491813+Tarru@users.noreply.github.com>
2019-05-31 17:17:12 +0200

    Create Diamond.java

---
+++ entity/src/main/java/entity/Diamond.java
@@ -0,0 +1,17 @@
+package entity;
+
+/**
+ * The class Diamond
+ * @author HugoDegrossi
+ */
+public class Diamond extends Object{
+    /**
+     * Contructor of Diamond
+     * @param name
+     * @param status
+     * @param lastMove
+     */
+    public Diamond(String name, boolean status, LastMove lastMove) {
+        super(name, status, lastMove);
+    }
+}
