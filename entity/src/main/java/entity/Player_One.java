3d3b7b5e70c28cbcbb88f6f883bbbddb4857cecf
Reinbold Valentin <44491813+Tarru@users.noreply.github.com>
2019-05-31 17:18:57 +0200

    Create Player_One.java

---
+++ entity/src/main/java/entity/Player_One.java
@@ -0,0 +1,16 @@
+package entity;
+/**
+ * The class Player_One
+ * @author HugoDegrossi
+ */
+public class Player_One extends Object {
+    /**
+     * Constructor
+     * @param name
+     * @param status
+     * @param lastMove
+     */
+    public Player_One(String name, boolean status, LastMove lastMove) {
+        super(name, status, lastMove);
+    }
+}
